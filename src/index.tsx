1c837ac89ba3f4787539549087180ebac53d6cc4
shaki <shakiralihamza05@gmail.com>
2022-01-14 23:52:18 +0500

    Added React Router v6

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import App from './App';
 import {store} from './app/store';
 import {Provider} from 'react-redux';
 import {CssBaseline} from "@mui/material";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <CssBaseline/>
-            <App/>
+            <BrowserRouter>
+                <CssBaseline/>
+                <App/>
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
