cb85ea6a02e4be74bce464825253fa1a1adb840c
shaki <shakiralihamza05@gmail.com>
2022-01-17 01:59:39 +0500

    updated entries component

--- src/features/entries/Entries.tsx
+++ src/features/entries/Entries.tsx
@@ -1,93 +1,53 @@
 import React from 'react';
 import {Box, Grid, Stack, Typography} from "@mui/material";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
 import PushPinIcon from '@mui/icons-material/PushPin';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-
+import EntriesListItem from "./EntriesListItem";
+import Scrollbars from "react-custom-scrollbars";
 
 function Entries() {
     return (
         <>
 
-            <Box sx={{
-                width: '100%',
-                backgroundImage: 'linear-gradient(to right, #2d2a2d, #2c292c)',
-                color: '#2c292c',
-                borderBottom: '1px solid',
-                borderColor: '#464348'
-            }}>
-                <Typography ml={1} sx={{color: '#a4a1a4', padding: '5px 0'}} fontSize={14}>
-                    <Stack direction={'row'}>
-                        <PushPinIcon sx={{fontSize: '12px', marginTop: '3px'}}/>
-                        <Box sx={{fontWeight: '400', fontSize: 12, marginLeft: '2px'}}>Pinned</Box>
-                    </Stack>
-                </Typography>
-            </Box>
             <Grid
                 container
                 sx={{
-                    padding: '10px 0',
-                    height: 'calc(100vh - 50px)',
+                    padding: '0 0 10px',
+                    height: 'calc(100vh - 37px)',
                     width: '100%'
                 }}
                 justifyContent={'space-between'}
                 alignItems={'stretch'}
                 direction={'column'}
             >
-                <Grid item>
-                    <Grid container>
-                        <List sx={{width: '100%'}}>
-                            {
-                                [1, 2, 3, 4, 5, 6].map((item) => (
-                                    <>
-                                        <ListItem alignItems="flex-start"
-                                                  sx={{
-                                                      ...(item === 4 && {backgroundImage: 'linear-gradient(to right, #3a373a, #373538)'}),
-                                                      ...(item === 5 && {backgroundImage: 'linear-gradient(to right, #ca8e20, #c98f20)'}),
-                                                      // color:'#e6bd6f'
-                                                  }}>
-                                            <ListItemText
-                                                sx={{marginLeft: '20px'}}
-                                                primary={
-                                                    <Typography
-                                                        sx={{fontWeight: '500', fontSize: '15px', color: '#e0dde0'}}
-                                                    >
-                                                        Brunch this weekend?
-                                                    </Typography>
-                                                }
-                                                secondary={
-                                                    <Box sx={{
-                                                        color: '#878588',
-                                                        ...(item === 5 && {color: '#e5cb98'})
-                                                    }}>
-                                                        {"I'll be in your neighborhood doing errands this…"}
-                                                    </Box>
-                                                }
-                                            />
-                                        </ListItem>
-                                        <Divider variant="inset" sx={{marginLeft: '37px'}} component="li"/>
-                                        {
-                                            item === 2 && <Divider sx={{border: '2px solid', borderColor:'#5e5b5e'}} component="li"/>
-                                        }
-                                    </>
-                                ))
-                            }
-
-                        </List>
-                    </Grid>
-
-                </Grid>
-
-                <Grid item>
-                    <Typography sx={{color: '#98a3b0'}} ml={2}>
-                        <Stack direction={'row'}>
-                            <AddCircleIcon/>&nbsp;
-                            <Box sx={{fontWeight: '400'}}>New Entry</Box>
-                        </Stack>
-                    </Typography>
+                <Grid item xs>
+                    <Scrollbars>
+                        <Box sx={{
+                            width: '100%',
+                            backgroundImage: 'linear-gradient(to right, #2d2a2d, #2c292c)',
+                            color: '#2c292c',
+                            borderBottom: '1px solid',
+                            borderColor: '#464348'
+                        }}>
+                            <Typography ml={1} sx={{color: '#a4a1a4', padding: '3px 0'}} fontSize={14}>
+                                <Stack direction={'row'}>
+                                    <PushPinIcon sx={{fontSize: '10px', marginTop: '3px'}}/>
+                                    <Box sx={{fontWeight: '400', fontSize: 10, marginLeft: '2px'}}>Pinned</Box>
+                                </Stack>
+                            </Typography>
+                        </Box>
+                        <Grid container>
+                            <List sx={{width: '100%', marginTop: '-10px'}}>
+                                {
+                                    [1, 2, 3, 6, 6, 6, 6, 6, 4, 5, 6].map((item, index, array) => (
+                                        <Box component={'span'} key={item} sx={{'&:hover': {cursor: 'default'}}}>
+                                            <EntriesListItem item={item} heading={''} description={''} index={index} arrayLength={array.length}/>
+                                        </Box>
+                                    ))
+                                }
+                            </List>
+                        </Grid>
+                    </Scrollbars>
                 </Grid>
             </Grid>
         </>
---
+++ src/features/entries/EntriesListItem.tsx
@@ -0,0 +1,63 @@
+import React, {FC} from 'react';
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import {Box, Typography} from "@mui/material";
+import Divider from "@mui/material/Divider";
+
+type ListItemProps = {
+    heading: string,
+    description: string,
+    item: number,
+    index: number,
+    arrayLength: number
+}
+const EntriesListItem: FC<ListItemProps> = ({item, index, arrayLength}) => (
+    <>
+        <ListItem alignItems="flex-start" tabIndex={0}
+                  sx={{
+                      ...(item === 4 && {backgroundImage: 'linear-gradient(to right, #3a373a, #373538)'}),
+                      '&: focus': {backgroundImage: 'linear-gradient(to right, #ca8e20, #c98f20)'},
+                      '&: focus .description': {color: '#e5cb98'},
+                      paddingTop: '5px',
+                      paddingBottom: '5px',
+                  }}>
+            <ListItemText
+                sx={{marginLeft: '20px'}}
+                primary={
+                    <Typography
+                        sx={{fontWeight: '500', fontSize: '13px', color: '#e0dde0'}}
+                    >
+                        Brunch this weekend?
+                    </Typography>
+                }
+                secondary={
+                    <>
+                        <Box
+                            className={'description'}
+                            component={'div'}
+                            sx={{
+                                color: '#878588',
+                                fontSize: '13px',
+                                textOverflow: 'ellipsis',
+                                overflow: 'hidden',
+                                whiteSpace: 'nowrap',
+                                maxWidth: '255px'
+                            }}
+                        >
+                            I'll be in your neighborhood doing errands this sefnsieufb
+                        </Box>
+                    </>
+                }
+            />
+        </ListItem>
+        {
+            index + 1 !== arrayLength &&
+            <Divider variant="inset" sx={{marginLeft: '37px'}} component="li"/>
+        }
+        {
+            item === 2 && <Divider sx={{border: '2px solid', borderColor: '#5e5b5e'}} component="li"/>
+        }
+    </>
+);
+
+export default EntriesListItem;
