5ac9fb5c14701cd6d10a9d42ce161867d9b9daaa
shaki <shakiralihamza05@gmail.com>
2022-01-22 06:35:03 +0500

    updated AddDiary component

--- src/features/diaries/AddDiary.tsx
+++ src/features/diaries/AddDiary.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import {closeMenu} from './addDiarySlice';
-import {useAppDispatch} from '../../app/hooks';
-import {Divider, Grid, IconButton, InputBase, Paper, styled, useTheme} from "@mui/material";
-import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import React, {useState} from 'react';
+import {useAppDispatch, useAppSelector} from '../../app/hooks';
+import {
+    Grid,
+    IconButton,
+    InputBase,
+    Paper,
+    styled,
+    ToggleButton,
+    ToggleButtonGroup,
+} from "@mui/material";
 import DoneOutlinedIcon from "@mui/icons-material/DoneOutlined";
+import {addDiary} from "./diariesSlice";
+import LockIcon from '@mui/icons-material/Lock';
+import PublicIcon from '@mui/icons-material/Public';
+import http from "../../services/api";
+import {Diary} from "../../interfaces/diary.interface";
+import {closeMenu} from "./addDiarySlice";
 
 const AddDiaryInput = styled(InputBase)(() => ({
     '& .MuiInputBase-input': {
         width: '100%',
         padding: '0 9px 0px 5px',
         fontSize: 14,
-        // Use the system font instead of the default Roboto font.
         fontFamily: [
             '-apple-system',
             'BlinkMacSystemFont',
@@ -26,9 +37,35 @@ const AddDiaryInput = styled(InputBase)(() => ({
     },
 }));
 
+type DiaryType = 'private' | 'public';
+
 const AddDiary = () => {
+    const [title, setTitle] = useState<string>('');
+    const [type, setType] = useState<DiaryType>('private');
+    const handleDiaryType = (event: any, val: DiaryType) => {
+        if (val !== null) {
+            setType(val)
+        }
+    }
     const dispatch = useAppDispatch();
-    const theme = useTheme();
+    const userId = useAppSelector(state => state.user?.id)
+    const [loading, setLoading] = useState(false);
+
+    const handleAddDiary = async () => {
+        setLoading(true)
+        const {diary} = await http.post<Partial<Diary>, { diary: Diary }>('/diaries/', {
+            title,
+            type,
+            userId,
+        });
+        if (diary) {
+            dispatch(addDiary([diary] as Diary[]));
+            dispatch(addDiary([diary] as Diary[]));
+            // dispatch(setUser(_user));
+            setLoading(false);
+            dispatch(closeMenu());
+        }
+    }
     return (
         <Grid item xs={12}>
             <Paper
@@ -42,16 +79,31 @@ const AddDiary = () => {
                     border: '1px solid',
                     borderColor: '#a5c4e1',
                     borderRadius: 1,
-                    backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
+                    backgroundColor:loading?'#000000':'#2b2b2b',
+                    opacity: loading? 0.2:1
                 }}
             >
-                <AddDiaryInput autoFocus/>
-                <IconButton size={'small'} onClick={() => dispatch(closeMenu())}>
-                    <CloseOutlinedIcon color={'warning'} sx={{fontSize: 16}}/>
-                </IconButton>
-                <Divider sx={{height: 18}} orientation="vertical"/>
-                <IconButton size={'small'}>
-                    <DoneOutlinedIcon color={'primary'} sx={{fontSize: 16}}/>
+                <AddDiaryInput
+                    autoFocus
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <ToggleButtonGroup
+                    value={type}
+                    exclusive
+                    onChange={handleDiaryType}
+                    size={"small"}
+                    sx={{margin: '6px 5px',}}
+                >
+                    <ToggleButton value="private" color={"primary"} sx={{padding: '0 5px'}} disableRipple>
+                        <LockIcon sx={{fontSize: 16}}/>
+                    </ToggleButton>
+                    <ToggleButton value="public" color={'primary'} sx={{padding: '0 5px'}} disableRipple>
+                        <PublicIcon sx={{fontSize: 16}}/>
+                    </ToggleButton>
+                </ToggleButtonGroup>
+                <IconButton size={'small'} onClick={handleAddDiary}>
+                    <DoneOutlinedIcon sx={{fontSize: 16}}/>
                 </IconButton>
             </Paper>
         </Grid>
